7d43d271ab7e4d097952230766bd321550e8e902
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-02-13 21:04:09 +0200

    [Zaichiku] Finished!

--- zaichiku/script.js
+++ zaichiku/script.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const question = document.getElementById('question');
+const answer = document.getElementById('answer');
+const letters = 'абвгґдеєжзиіїйклммнопрстуфхцчшщьюя';
+setTimeout(() => {
+  question.setAttribute('style', 'display:block');
+}, 4000);
+question.addEventListener('click', () => {
+  const randomizer = setInterval(() => {
+    let word = '';
+    for (let i = 0; i < 7; i++) {
+      word += letters[Math.floor(Math.random() * 30)];
+    }
+    answer.innerHTML = word;
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(randomizer);
+    answer.innerHTML = 'Сумочка!';
+    question.setAttribute('class', 'decrease');
+    answer.setAttribute('class', 'increase');
+    question.removeEventListener('click');
+  }, 3000);
+});
